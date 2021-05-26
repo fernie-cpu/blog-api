ddf1263d174b28ce73b3e945c592ee40bc27d863
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-26 16:53:21 -0300

    passport strategy

---
+++ passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    function (email, password, cb) {
+      return UserModel.findOne({ email, password })
+        .then((user) => {
+          if (!user) {
+            return cb(null, false, { message: 'incorrect email or password' });
+          }
+
+          return cb(null, user, { message: 'logged in successfully' });
+        })
+        .catch((err) => cb(err));
+    }
+  )
+);
