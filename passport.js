cdccddcae23508ca0503dadd0a6ceb107731d26d
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-27 16:14:07 -0300

    passaport jwt config

--- passport.js
+++ passport.js
@@ -1,22 +1,66 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const User = require('./models/user');
+require('dotenv').config();
 
 passport.use(
+  'signup',
+  new LocalStrategy(
+    { usernameField: 'username', passwordField: 'password' },
+    async (username, password, done) => {
+      try {
+        const user = await User.create({ username, password });
+
+        return done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  'login',
   new LocalStrategy(
     {
-      usernameField: 'email',
+      usernameField: 'username',
       passwordField: 'password',
     },
-    function (email, password, cb) {
-      return UserModel.findOne({ email, password })
-        .then((user) => {
-          if (!user) {
-            return cb(null, false, { message: 'incorrect email or password' });
-          }
+    async function (username, password, done) {
+      try {
+        const user = await User.findOne({ username });
 
-          return cb(null, user, { message: 'logged in successfully' });
-        })
-        .catch((err) => cb(err));
+        if (!user) {
+          return done(null, false, { message: 'user not found' });
+        }
+
+        const validate = await user.isValidPassword(password);
+        if (!validate) {
+          return done(null, user, { message: 'wrong password' });
+        }
+
+        return done(null, user, { message: 'login successful' });
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET,
+    },
+    async function (token, done) {
+      try {
+        return done(null, token.user);
+      } catch (err) {
+        done(err);
+      }
     }
   )
 );
